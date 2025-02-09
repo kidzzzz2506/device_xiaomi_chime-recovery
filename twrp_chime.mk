ae63f18032357c59fc4ea4a738a07ede6c8fa2ca
kidzzzz2506 <kidihariadi123@gmail.com>
2025-02-09 21:58:15 +0700

    rename TWRP version

--- twrp_chime.mk
+++ twrp_chime.mk
@@ -33,5 +33,5 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6115
 PRODUCT_MANUFACTURER := Xiaomi
 
-# hani TWRP version
-TW_DEVICE_VERSION  := hani
+# Kidzz TWRP version
+TW_DEVICE_VERSION  := Kidzz
